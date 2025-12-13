0a90575c6996121c754795d884ac7445b86affbb
Fabio Gabelli <f.gabelli@MacBookPro.homenet.telecomitalia.it>
2025-12-13 16:07:17 +0100

    fix: Add missing model imports to safety and sos services

--- lib/core/services/safety_service.dart
+++ lib/core/services/safety_service.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../shared/models/safety_alert_model.dart'; // Added
 import '../shared/models/safety_alert_model.dart';
 
 class SafetyService {
--- lib/core/services/sos_service.dart
+++ lib/core/services/sos_service.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../shared/models/lost_pet_alert_model.dart'; // Added
 import '../shared/models/lost_pet_alert_model.dart';
 
 class SOSService {
